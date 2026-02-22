81f8fced8e128cc78ab72251bc151913b2619b40
DevMathAvila <avilaestudos@outlook.com>
2026-02-21 22:22:34 -0300

    Feat: Adicionado algumas modificações no metadado para WHATSAPP

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import '@/data/styles/globals.css';
 import { Inter } from 'next/font/google';
-
-// No Next.js, removemos o ".js" do final do import
 import Navbar from '@/components/layout/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -21,24 +19,40 @@ export const metadata = {
     'Condomínios de Luxo Indaiatuba',
     'Aprovação de projetos prefeitura Indaiatuba',
     'Reforma residencial alto padrão',
-    'Design de interiores Indaiatuba'
+    'Design de interiores Indaiatuba',
+    'Engenharia Indaiatuba',
+    'Mayara Engenharia',
+    'Gaspareto Engenharia',
+    'Mayara Gaspareto Engenharia',
+    'Arquiteta indaiatuba'
   ],
   authors: [{ name: 'Mayara Gaspareto' }],
   creator: 'Mayara Gaspareto',
+  openGraph: {
+    title: 'Mayara Gaspareto | Arquitetura & Engenharia',
+    description: 'Projetos autorais e engenharia de precisão em Indaiatuba e região.',
+    url: 'https://www.mayaragaspareto.com.br',
+    siteName: 'Mayara Gaspareto',
+    images: [
+      {
+        url: 'https://www.mayaragaspareto.com.br/images/aprovacao/approve.jpg',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className="scroll-smooth">
       <body className={`${inter.className} bg-off-white text-dark-text flex flex-col min-h-screen`}>
-        {/* Navbar fixa em todas as páginas */}
         <Navbar /> 
-        
         <main className="flex-grow">
           {children}
         </main>
-
-        {/* Sem Footer por enquanto, o flex-grow garante que o layout não quebre */}
       </body>
     </html>
   );
